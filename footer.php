42f98644238ac389a136f3ecba8d8e0ac9bd938c
Ayesha <ayeshaakhter222@gmail.com>
2021-03-20 00:31:30 +0600

    modification in footer

--- footer.php
+++ footer.php
@@ -1,10 +1,22 @@
 <footer>
     <div class="row">
-        <div class="col-sm-6">
+        <div class="col-sm-4">
             <small>Copyright © 2021</small>
         </div>
-        <div class="col-sm-6 text-end">
+        <div class="col-sm-4 text-end">
             <small><b>Developed by</b> Ayesha Siddika & Ajajul Islam (Wjan)</small>
+        </div>
+        <div class="col-sm-4 text-end">
+            <p class="mb-0">
+                <small>
+                    <b>Email</b> <a href="mailto:ayeshaakhter222@gmail.com">ayeshaakhter222@gmail.com</a>
+                </small>
+            </p>
+            <p class="mb-0">
+                <small>
+                    <b>Email</b> <a href="mailto:ajajulislam3@gmail.com">ajajulislam3@gmail.com</a>
+                </small>
+            </p>
         </div>
     </div>
 </footer>
