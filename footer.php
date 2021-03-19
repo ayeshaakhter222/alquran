323f1e69d324db8d9083db76b0a7fef5b4956ed3
Ayesha <ayeshaakhter222@gmail.com>
2021-03-19 23:25:45 +0600

    Initalize Project

---
+++ footer.php
@@ -0,0 +1,10 @@
+<footer>
+    <div class="row">
+        <div class="col-sm-6">
+            <small>Copyright © 2021</small>
+        </div>
+        <div class="col-sm-6 text-end">
+            <small><b>Developed by</b> Ayesha Siddika & Ajajul Islam (Wjan)</small>
+        </div>
+    </div>
+</footer>
